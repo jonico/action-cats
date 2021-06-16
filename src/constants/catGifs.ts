4e31675724abac6594c8d94b57eff5bda76b95d3
Johannes Nicolai <jonico@github.com>
2021-06-16 07:36:19 +0000

    Use hard coded images

--- src/constants/catGifs.ts
+++ src/constants/catGifs.ts
@@ -4,17 +4,17 @@
  * linking issues.
  */
 const catGifs: string[] = [
-  'https://github.com/ruairidhwm/action-cats/blob/master/assets/1.gif',
-  'https://github.com/ruairidhwm/action-cats/blob/master/assets/2.gif',
-  'https://github.com/ruairidhwm/action-cats/blob/master/assets/3.gif',
-  'https://github.com/ruairidhwm/action-cats/blob/master/assets/4.gif',
-  'https://github.com/ruairidhwm/action-cats/blob/master/assets/5.gif',
-  'https://github.com/ruairidhwm/action-cats/blob/master/assets/6.gif',
-  'https://github.com/ruairidhwm/action-cats/blob/master/assets/7.gif',
-  'https://github.com/ruairidhwm/action-cats/blob/master/assets/8.gif',
-  'https://github.com/ruairidhwm/action-cats/blob/master/assets/9.gif',
-  'https://github.com/ruairidhwm/action-cats/blob/master/assets/10.gif',
-  'https://github.com/ruairidhwm/action-cats/blob/master/assets/11.gif'
+  'https://media.giphy.com/media/VbnUQpnihPSIgIXuZv/giphy.gif',
+  'https://media.giphy.com/media/SRO0ZwmImic0/giphy.gif',
+  'https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif',
+  'https://media.giphy.com/media/y9hjvnO2bwJbO/giphy.gif',
+  'https://media.giphy.com/media/QyJ0We4GHpjBa7PvKL/giphy.gif',
+  'https://media.giphy.com/media/cfuL5gqFDreXxkWQ4o/giphy.gif',
+  'https://media.giphy.com/media/Ov5NiLVXT8JEc/giphy.gif',
+  'https://media.giphy.com/media/3o72EX5QZ9N9d51dqo/giphy.gif',
+  'https://media.giphy.com/media/H4DjXQXamtTiIuCcRU/giphy.gif',
+  'https://media.giphy.com/media/dIVa0pwco4Mj5rQ7gy/giphy.gif',
+  'https://media.giphy.com/media/l2JJDdD7cv4xdGGis/giphy.gif'
 ]
 
 export default catGifs
